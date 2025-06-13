716a04efb7dbde8a8aef8f03a66088ec540e9c64
alejandroQtx <qualitrain@gmail.com>
2025-06-13 13:36:46 -0600

    proy vacio

---
+++ src/main/java/mx/com/qtx/ejmSpSec/Mod05DemoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package mx.com.qtx.ejmSpSec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod05DemoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mod05DemoSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/qtx/ejmSpSec/Mod05DemoSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.qtx.ejmSpSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod05DemoSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
