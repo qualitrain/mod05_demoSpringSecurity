710b76be91eaf2e52aff5c19892b03cd6525995b
alejandroQtx <qualitrain@gmail.com>
2025-06-18 19:49:09 -0600

    autenticacion en memoria ok

---
+++ src/main/java/mx/com/qtx/ejmSpSec/ConfiguracionSeguridad.java
@@ -0,0 +1,52 @@
+package mx.com.qtx.ejmSpSec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracionSeguridad {
+	@Bean
+	SecurityFilterChain crearSecurityFilterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests(autorizador->autorizador.anyRequest().authenticated())
+		     .httpBasic(Customizer.withDefaults())
+		     .formLogin(Customizer.withDefaults())
+		     .build();
+	}
+	
+	@Bean
+	UserDetailsService crearGestorDeUsuarios() {
+		 UserDetails usuarioAlex = User.withDefaultPasswordEncoder()
+		     .username("Alex")
+		     .password("estrellamarinera")
+		     .roles("ADMIN","VTAS")
+		     .build();
+		 
+		 UserDetails usuarioEdgar = User.withDefaultPasswordEncoder()
+			     .username("Edgar")
+			     .password("perroamarillo")
+			     .roles("COMPRAS")
+			     .build();
+		 
+		 UserDetails usuarioRosalinda = User.withDefaultPasswordEncoder()
+			     .username("Rosalinda")
+			     .password("tortugaazul")
+			     .roles("DESARROLLO")
+			     .build();
+		 
+		InMemoryUserDetailsManager gestorUsuarios = new InMemoryUserDetailsManager(usuarioRosalinda, 
+				                                                                   usuarioEdgar, 
+				                                                                   usuarioAlex);
+		return gestorUsuarios;
+	}
+	
+}
