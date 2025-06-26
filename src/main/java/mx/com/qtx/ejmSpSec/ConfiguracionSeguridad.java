968530bfa9dbb7e07f1c147aa530bfd4c21d9bf7
alejandroQtx <qualitrain@gmail.com>
2025-06-26 12:33:39 -0600

    autenticacion bd esquema personalizado

--- src/main/java/mx/com/qtx/ejmSpSec/ConfiguracionSeguridad.java
+++ src/main/java/mx/com/qtx/ejmSpSec/ConfiguracionSeguridad.java
@@ -2,23 +2,29 @@ package mx.com.qtx.ejmSpSec;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 @EnableWebSecurity
 public class ConfiguracionSeguridad {
+	private static Logger bitacora = LoggerFactory.getLogger(ConfiguracionSeguridad.class);
+	
 	@Bean
 	SecurityFilterChain crearSecurityFilterChain(HttpSecurity http) throws Exception {
 		return http.authorizeHttpRequests(autorizador->autorizador
@@ -60,7 +66,7 @@ public class ConfiguracionSeguridad {
 		return gestorUsuarios;
 	}
 	
-    @Bean
+    //@Bean
     UserDetailsManager getGestorBdUsuarios(DataSource dataSource) {
         UserDetails usuarioAlex = User.withUsername("betote")
                  .password("{bcrypt}$2a$10$rz1ehHUZCmkf2zdQ0cZQsefzyOJmHb2NeD2gPmeQjMZPqXcjEYLEe")
@@ -88,5 +94,28 @@ public class ConfiguracionSeguridad {
         
         return gestorUsuariosBD;
    }
-	
+    @Bean
+   UserDetailsService getGestorBdUsuariosPersonalizada(DataSource dataSource) {
+               bitacora.trace("getGestorBdUsuariosPersonalizada()");
+               HikariDataSource hds = (HikariDataSource) dataSource;
+               bitacora.debug("Se ha instanciado data source mysql que apunta a BD:" + hds.getJdbcUrl());
+               
+           //Se usa una BD Personalizada. Ya debe contener los datos de usuarios y roles
+               final String QUERY_DATOS_USUARIO_X_NOMBRE = "SELECT usr_nombre, usr_paswd, usr_habilitado "
+                               + "FROM usuario WHERE usr_nombre = ?";
+
+               //Los roles deben estar escritos en los registros de la base de datos con el prefijo "ROLE_"
+               //Por ejemplo, ROLE_AGENTE o ROLE_LOGISTICA
+               final String QUERY_ROLES_X_USUARIO ="SELECT usr_nombre, aut_nombre "
+                               + "FROM usuario, autoridad "
+                               + "WHERE usr_nombre = ? "
+                               + "AND usr_nombre = aut_nombre_usr";
+               
+               JdbcDaoImpl gestorBdUsuariosPersonalizada = new JdbcDaoImpl();
+               gestorBdUsuariosPersonalizada.setDataSource(dataSource);
+               gestorBdUsuariosPersonalizada.setUsersByUsernameQuery(QUERY_DATOS_USUARIO_X_NOMBRE);
+               gestorBdUsuariosPersonalizada.setAuthoritiesByUsernameQuery(QUERY_ROLES_X_USUARIO);
+               
+               return gestorBdUsuariosPersonalizada;
+   }	
 }
